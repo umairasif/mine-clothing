010c52f8f8b9ef58592347dfe224f952a8fcc958
Umair Asif <umair.asif@manafatech.com>
2024-10-21 19:00:21 +0500

    directory component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import "./categories.style.scss";
-import CategoryItem from "./components/category-item/category-item.component";
+import Directory from "./components/directory/directory.component";
 
 const App = () => {
   const categories = [
@@ -30,13 +30,7 @@ const App = () => {
     },
   ];
 
-  return (
-    <div className="categories-container">
-      {categories.map((category) => (
-        <CategoryItem key={category.id} category={category} />
-      ))}
-    </div>
-  );
+  return <Directory categories={categories} />;
 };
 
 export default App;
