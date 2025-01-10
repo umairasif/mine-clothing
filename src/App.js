ccc6f38d1fc48adec93662965a43ff1e22789558
umairasif <umair.asif@sukuktech.com>
2025-01-10 17:14:28 +0500

    react router link

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import "./categories.style.scss";
 import Home from "./routes/home/home.component";
 import { Routes, Route } from "react-router-dom";
+import Navigation from "./routes/navigation/navigation.component";
 
 const Shop = () => {
   return (
@@ -12,8 +13,9 @@ const Shop = () => {
 const App = () => {
   return (
     <Routes>
-      <Route path="/home" element={<Home />}>
-        <Route path="shop" element={<Shop />} />
+      <Route path='/' element={<Navigation />}>
+        <Route index element={<Home />} />
+        <Route path="shop" element={<Shop />} />      
       </Route>
     </Routes>
   );
